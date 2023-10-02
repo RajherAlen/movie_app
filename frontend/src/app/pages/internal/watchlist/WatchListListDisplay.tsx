64c73b2fe8e8ce10d8281d0a92298c6d0efb90a1
Alen Rajher <alen.rajher@gmail.com>
2023-10-02 22:35:59 +0200

    added status and todo

--- frontend/src/app/pages/internal/watchlist/WatchListListDisplay.tsx
+++ frontend/src/app/pages/internal/watchlist/WatchListListDisplay.tsx
@@ -20,14 +20,25 @@ const WatchListListDisplay = () => {
 
     if (data?.length === 0 || !data) return <div>List is empty</div>;
 
+    // TODO
+    // ADD FILTERS FOR WATCHED | NOT WATCHED
+
+    // PAGINATION
+
+    // SEARCH
+
+    // SORT BY VOTE | ALPHABETICAL
+
+    // LINK TO PREVIEW
+
     return (
         <Table>
             <TableHeader>
                 <TableRow>
                     <TableHead className="w-[100px]">Title</TableHead>
                     <TableHead>Overview</TableHead>
-                    <TableHead className='text-right'>Vote</TableHead>
-                    {/* <TableHead className="text-right">Actions</TableHead> */}
+                    <TableHead className="text-center">Status</TableHead>
+                    <TableHead className="text-right">Vote</TableHead>
                 </TableRow>
             </TableHeader>
             <TableBody>
@@ -38,6 +49,14 @@ const WatchListListDisplay = () => {
                         </TableCell>
                         <TableCell className="min-w-[600px] max-w-[600px] truncate whitespace-nowrap overflow-hidden">
                             {movie.overview}
+                        </TableCell>
+                        <TableCell className="text-center">
+                            {/* <div className="py-1 px-4  inline shadow-md no-underline rounded-full bg-red-700 text-white text-xs font-medium">
+                                Not Watched
+                            </div> */}
+                            <div className="py-1 px-4  inline shadow-md no-underline rounded-full bg-green-600 text-white text-xs font-medium">
+                                Watched
+                            </div>
                         </TableCell>
                         <TableCell className="text-right">
                             {movie.vote_average}
